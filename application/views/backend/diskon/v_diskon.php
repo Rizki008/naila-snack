bb54d2fe55f713d0c3305732b562425685b48458
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-09-23 00:54:54 +0700

    view diskon member

---
+++ application/views/backend/diskon/v_diskon.php
@@ -0,0 +1,57 @@
+<div class="col-md-12">
+	<div class="card card-primary">
+		<div class="card-header">
+			<h3 class="card-title">Data Produk</h3>
+
+			<div class="card-tools">
+				<a href="<?= base_url('master_produk/createDiskon') ?>" type="button" class="btn btn-primary btn-sm"><i class="fas fa-plus"></i>
+					Tambah Produk</a>
+			</div>
+			<!-- /.card-tools -->
+		</div>
+		<!-- /.card-header -->
+		<div class="card-body">
+			<?php
+			if ($this->session->flashdata('pesan')) {
+				echo '<div class="alert alert-success alert-dismissible">
+                    <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                    <h5><i class="icon fas fa-check"></i>';
+				echo $this->session->flashdata('pesan');
+				echo '</h5></div>';
+			}
+			?>
+			<table id="example1" class="table table-bordered" id="example1">
+				<thead class="text-center">
+					<tr>
+						<th>No</th>
+						<th>Nama Produk</th>
+						<th>Nama Diskon</th>
+						<th>Besar Diskon</th>
+						<th>Deskripsi</th>
+						<th>Member</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php $no = 1;
+					foreach ($diskon as $key => $value) { ?>
+						<tr class="text-center">
+							<td><?= $no++; ?></td>
+							<td><?= $value->nama_produk ?></td>
+							<td><?= $value->nama_diskon ?></td>
+							<td><?= number_format($value->diskon, 0) ?> %</td>
+							<td><?= $value->tgl_selesai ?></td>
+							<td><?= $value->member ?></td>
+							<td>
+								<a href="<?= base_url('master_produk/update/' . $value->id_produk) ?>" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i></a>
+								<a href="<?= base_url('master_produk/delete/' . $value->id_produk) ?>" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></a>
+							</td>
+						</tr>
+					<?php } ?>
+				</tbody>
+			</table>
+		</div>
+		<!-- /.card-body -->
+	</div>
+	<!-- /.card -->
+</div>
---
+++ application/views/backend/diskon/v_edit.php
@@ -0,0 +1,96 @@
+<!-- right column -->
+<div class="col-md-12">
+	<!-- general form elements disabled -->
+	<div class="card card-primary">
+		<div class="card-header">
+			<h3 class="card-title">Form Edit Barang</h3>
+		</div>
+		<!-- /.card-header -->
+		<div class="card-body">
+			<?php
+			//notifikasi form kosong
+			echo validation_errors('<div class="alert alert-warning alert-dismissible">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            <h5><i class="icon fas fa-exclamation-triangle"></i>', '</h5></div>');
+
+			//notifikasi gagal upload gambar
+			if (isset($error_upload)) {
+				echo '<div class="alert alert-warning alert-dismissible">
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+                <h5><i class="icon fas fa-exclamation-triangle"></i>' . $error_upload . '</h5></div>';
+			}
+			echo form_open('master_produk/update') ?>
+			<!-- text input -->
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="form-group">
+						<label>Nama Produk</label>
+						<input name="nama_diskon" type="text" class="form-control" placeholder="Nama Produk" value="<?= $diskon->nama_diskon ?>">
+					</div>
+				</div>
+
+				<div class="col-sm-4">
+					<div class="form-group">
+						<label>Nama Produk</label>
+						<select id="produk-diskon" name="produk" class="form-control">
+							<option value="">---Pilih Kategori Produk---</option>
+							<?php
+							foreach ($produk as $key => $value) {
+							?>
+								<option data-harga="<?= number_format($value->harga, 0) ?>" value="<?= $value->id_produk ?>" <?php if ($diskon->id_produk == $value->id_produk) {
+																																	echo 'selected';
+																																} ?>><?= $value->nama_produk ?></option>
+							<?php
+							}
+							?>
+						</select>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<div class="form-group">
+						<label>Level Member</label>
+						<select id="produk-diskon" name="level" class="form-control">
+							<option value="">---Pilih Level Member---</option>
+							<option value="1" <?php if ($diskon->member == '1') {
+													echo 'selected';
+												} ?>>Gold</option>
+							<option value="2" <?php if ($diskon->member == '2') {
+													echo 'selected';
+												} ?>>Silver</option>
+							<option value="3" <?php if ($diskon->member == '3') {
+													echo 'selected';
+												} ?>>Clasic</option>
+						</select>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<!-- text input -->
+					<div class="form-group">
+						<label>Harga Produk</label>
+						<input name="diskon" type="number" class="form-control" placeholder="Besar Diskon" value="<?= $diskon->diskon ?>">
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<!-- text input -->
+					<div class="form-group">
+						<label>Tanggal Selesai</label>
+						<input name="tgl" type="date" class="form-control" placeholder="Tanggal Selesai" value="<?= $diskon->tgl_selesai ?>">
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<!-- text input -->
+					<div class="form-group">
+						<label>Harga Produk</label>
+						<input name="harga" type="text" min="0" class="harga form-control" placeholder="Harga Produk" value="<?= $diskon->harga ?>" readonly>
+					</div>
+				</div>
+			</div>
+			<div class="form-group">
+				<button type="submit" class="btn btn-primary btn-sm">Simpan</button>
+				<a href="<?= base_url('master_produk/produk') ?>" class="btn btn-warning btn-sm">Kembali</a>
+			</div>
+
+			<?php echo form_close() ?>
+		</div>
+	</div>
+</div>
