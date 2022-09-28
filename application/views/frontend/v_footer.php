c66f71e61df3372f8dbdf1fc96d2b56835ba9324
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-09-28 10:10:11 +0700

    mengubah footer

--- application/views/frontend/v_footer.php
+++ application/views/frontend/v_footer.php
@@ -37,12 +37,12 @@
             </div>
             <div class="col-lg-4 col-md-12">
                 <div class="footer__widget">
-                    <h6>Join Our Newsletter Now</h6>
-                    <p>Get E-mail updates about our latest shop and special offers.</p>
-                    <form action="#">
+                    <!-- <h6>Join Our Newsletter Now</h6>
+                    <p>Get E-mail updates about our latest shop and special offers.</p> -->
+                    <!-- <form action="#">
                         <input type="text" placeholder="Enter your mail">
                         <button type="submit" class="site-btn">Subscribe</button>
-                    </form>
+                    </form> -->
                     <div class="footer__widget__social">
                         <a href="#"><i class="fa fa-facebook"></i></a>
                         <a href="#"><i class="fa fa-instagram"></i></a>
@@ -60,7 +60,7 @@
                             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                             Copyright &copy;<script>
                                 document.write(new Date().getFullYear());
-                            </script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                            </script> <i class="fa fa-heart" aria-hidden="true"></i> by <a href="#" target="_blank">Naila Snack & Catering</a>
                             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
                         </p>
                     </div>
