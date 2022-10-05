3d7c6090dc880c82a009b57fdca5f4aca9093ff1
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-10-05 10:13:40 +0700

    menambahkan menu analisis di navbar

--- application/views/backend/v_nav.php
+++ application/views/backend/v_nav.php
@@ -13,7 +13,7 @@
 		<!-- Sidebar user panel (optional) -->
 		<div class="user-panel mt-3 pb-3 mb-3 d-flex">
 			<div class="image">
-				<img src="<?= base_url() ?>template/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+				<img src="<?= base_url() ?>template/dist/img/logos.png" class="img-circle elevation-2" alt="User Image">
 			</div>
 			<div class="info">
 				<a href="<?= base_url('admin') ?>" class="d-block">
@@ -124,6 +124,41 @@
 							Laporan
 						</p>
 					</a>
+				</li>
+				<li class="nav-item has-treeview">
+					<a href="#" class="nav-link">
+						<i class="nav-icon fas fa-chart-bar"></i>
+						<p>
+							Data Analisis
+							<i class="right fas fa-angle-left"></i>
+						</p>
+					</a>
+					<ul class="nav nav-treeview">
+						<li class="nav-item">
+							<a href="<?= base_url('analisis/produk') ?>" class="nav-link">
+								<i class="far fa-circle nav-icon"></i>
+								<p>Analisis Produk</p>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="<?= base_url('analisis/pelanggan') ?>" class="nav-link">
+								<i class="far fa-circle nav-icon"></i>
+								<p>Analisis Pelanggan</p>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="<?= base_url('analisis/uang') ?>" class="nav-link">
+								<i class="far fa-circle nav-icon"></i>
+								<p>Analisis Keuangan</p>
+							</a>
+						</li>
+						<li class="nav-item">
+							<a href="<?= base_url('analisis/alamat') ?>" class="nav-link">
+								<i class="far fa-circle nav-icon"></i>
+								<p>Analisis Alamat</p>
+							</a>
+						</li>
+					</ul>
 				</li>
 
 				<li class="nav-item">
