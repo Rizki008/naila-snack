39b24b8aa187486b6e643f0b2d1dbae81f1664ac
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-10-05 11:50:08 +0700

    mengubah images header

--- application/views/frontend/pelanggan/v_login.php
+++ application/views/frontend/pelanggan/v_login.php
@@ -45,7 +45,7 @@
 <!-- Hero Section End -->
 
 <!-- Breadcrumb Section Begin -->
-<section class="breadcrumb-section set-bg" data-setbg="<?= base_url() ?>ogani-master/img/breadcrumb.jpg">
+<section class="breadcrumb-section set-bg" data-setbg="<?= base_url() ?>ogani-master/img/breadcrumb1.jpeg">
     <div class="container">
         <div class="row">
             <div class="col-lg-12 text-center">
