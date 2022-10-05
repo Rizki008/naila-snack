b0699bef5ac2e18e78741966b1c64b4f3fdd01c5
Rizki008 <muhammadrizkinurhamdani@gmail.com>
2022-10-05 10:10:18 +0700

    menambahkan view analisis hari bulan tahun (produk,alamat,pelanggan,uang)

---
+++ application/views/backend/analisis/alamat/v_alamat_bulanan.php
@@ -0,0 +1,133 @@
+<section class="col-lg-10 connectedSortable">
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">
+                <i class="fas fa-chart-pie mr-1"></i>
+                Analisis Alamat
+            </h3>
+            <div class="card-tools">
+                <ul class="nav nav-pills ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#revenue-chart" data-toggle="tab">Analisis Alamat</a>
+                    </li>
+                </ul>
+            </div>
+        </div><!-- /.card-header -->
+        <div class="card-body">
+            <div class="tab-content p-0">
+                <!-- Morris chart - Sales -->
+                <div class="chart tab-pane active" id="revenue-chart" style="position: relative; height: 300px;">
+                    <?php
+                    foreach ($analisis_alamat as $key => $value) {
+                        $kecamatan[] = $value->kecamatan;
+                        $qty[] = $value->qty;
+                    }
+                    ?>
+                    <canvas id="mymaharani" height="100" style="height: 100px;"></canvas>
+                    <!-- <canvas id="mymaharani" height="100"></canvas> -->
+                    <script>
+                        var ctx = document.getElementById('mymaharani');
+                        var mymaharani = new Chart(ctx, {
+                            type: 'bar',
+                            data: {
+                                labels: <?= json_encode($kecamatan) ?>,
+                                datasets: [{
+                                    label: 'Grafik Nominal Uang Terbesar',
+                                    data: <?= json_encode($qty) ?>,
+                                    backgroundColor: [
+                                        'rgba(255, 99, 132, 0.80)',
+                                        'rgba(54, 162, 235, 0.80)',
+                                        'rgba(255, 206, 86, 0.80)',
+                                        'rgba(75, 192, 192, 0.80)',
+                                        'rgba(153, 102, 255, 0.80)',
+                                        'rgba(255, 159, 64, 0.80)',
+                                        'rgba(201, 76, 76, 0.3)',
+                                        'rgba(201, 77, 77, 1)',
+                                        'rgba(0, 140, 162, 1)',
+                                        'rgba(158, 109, 8, 1)',
+                                        'rgba(201, 76, 76, 0.8)',
+                                        'rgba(0, 129, 212, 1)',
+                                        'rgba(201, 77, 201, 1)',
+                                        'rgba(255, 207, 207, 1)',
+                                        'rgba(201, 77, 77, 1)',
+                                        'rgba(128, 98, 98, 1)',
+                                        'rgba(0, 0, 0, 1)',
+                                        'rgba(128, 128, 128, 1)',
+                                        'rgba(255, 99, 132, 0.80)',
+                                        'rgba(54, 162, 235, 0.80)',
+                                        'rgba(255, 206, 86, 0.80)',
+                                        'rgba(75, 192, 192, 0.80)',
+                                        'rgba(153, 102, 255, 0.80)',
+                                        'rgba(255, 159, 64, 0.80)',
+                                        'rgba(201, 76, 76, 0.3)',
+                                        'rgba(201, 77, 77, 1)',
+                                        'rgba(0, 140, 162, 1)',
+                                        'rgba(158, 109, 8, 1)',
+                                        'rgba(201, 76, 76, 0.8)',
+                                        'rgba(0, 129, 212, 1)',
+                                        'rgba(201, 77, 201, 1)',
+                                        'rgba(255, 207, 207, 1)',
+                                        'rgba(201, 77, 77, 1)',
+                                        'rgba(128, 98, 98, 1)',
+                                        'rgba(0, 0, 0, 1)',
+                                        'rgba(128, 128, 128, 1)'
+                                    ],
+                                    borderColor: [
+                                        'rgba(255, 99, 132, 1)',
+                                        'rgba(54, 162, 235, 1)',
+                                        'rgba(255, 206, 86, 1)',
+                                        'rgba(75, 192, 192, 1)',
+                                        'rgba(153, 102, 255, 1)',
+                                        'rgba(255, 159, 64, 1)',
+                                        'rgba(201, 76, 76, 0.3)',
+                                        'rgba(201, 77, 77, 1)',
+                                        'rgba(0, 140, 162, 1)',
+                                        'rgba(158, 109, 8, 1)',
+                                        'rgba(201, 76, 76, 0.8)',
+                                        'rgba(0, 129, 212, 1)',
+                                        'rgba(201, 77, 201, 1)',
+                                        'rgba(255, 207, 207, 1)',
+                                        'rgba(201, 77, 77, 1)',
+                                        'rgba(128, 98, 98, 1)',
+                                        'rgba(0, 0, 0, 1)',
+                                        'rgba(128, 128, 128, 1)',
+                                        'rgba(255, 99, 132, 1)',
+                                        'rgba(54, 162, 235, 1)',
+                                        'rgba(255, 206, 86, 1)',
+                                        'rgba(75, 192, 192, 1)',
+                                        'rgba(153, 102, 255, 1)',
+                                        'rgba(255, 159, 64, 1)',
+                                        'rgba(201, 76, 76, 0.3)',
+                                        'rgba(201, 77, 77, 1)',
+                                        'rgba(0, 140, 162, 1)',
+                                        'rgba(158, 109, 8, 1)',
+                                        'rgba(201, 76, 76, 0.8)',
+                                        'rgba(0, 129, 212, 1)',
+                                        'rgba(201, 77, 201, 1)',
+                                        'rgba(255, 207, 207, 1)',
+                                        'rgba(201, 77, 77, 1)',
+                                        'rgba(128, 98, 98, 1)',
+                                        'rgba(0, 0, 0, 1)',
+                                        'rgba(128, 128, 128, 1)'
+                                    ],
+                                    fill: false,
+                                    borderWidth: 1
+                                }]
+                            },
+                            options: {
+                                scales: {
+                                    yAxes: [{
+                                        ticks: {
+                                            beginAtZero: true
+                                        }
+                                    }]
+                                }
+                            }
+                        });
+                    </script>
+                </div>
+            </div>
+        </div><!-- /.card-body -->
+    </div>
+    <!-- /.card -->
+</section>
---
+++ application/views/backend/analisis/v_analisis.php
@@ -0,0 +1,156 @@
+<div class="col-md-4">
+	<div class="card card-primary">
+		<div class="card-header">
+			<h3 class="card-title">Analisis Produk Harian</h3>
+		</div>
+		<!-- /.card-header -->
+		<div class="card-body">
+			<?php
+			echo form_open('analisis/produk_hari') ?>
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="form-group">
+						<label>Tanggal</label>
+						<select name="tanggal" class="form-control">
+							<?php
+							$mulai = 1;
+							for ($i = $mulai; $i < $mulai + 31; $i++) {
+								$sel = $i == date('Y') ? 'selected="selected"' : '';
+								echo '<option value="' . $i . '"' . $sel . '>' . $i . '</option>';
+							}
+							?>
+						</select>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<div class="form-group">
+						<label>Bulan</label>
+						<select name="bulan" class="form-control">
+							<?php
+							$mulai = 1;
+							for ($i = $mulai; $i < $mulai + 12; $i++) {
+								$sel = $i == date('Y') ? 'selected="selected"' : '';
+								echo '<option value="' . $i . '"' . $sel . '>' . $i . '</option>';
+							}
+							?>
+						</select>
+					</div>
+				</div>
+				<div class="col-sm-4">
+					<div class="form-group">
+						<label>Tahun</label>
+						<select name="tahun" class="form-control">
+							<?php
+							$mulai = date('Y') - 1;
+							for ($i = $mulai; $i < $mulai + 10; $i++) {
+								$sel = $i == date('Y') ? 'selected="selected"' : '';
+								echo '<option value="' . $i . '"' . $sel . '>' . $i . '</option>';
+							}
+							?>
+						</select>
+					</div>
+				</div>
+				<div class="col-sm-12">
+					<div class="form-group">
+						<button type="submit" class="btn btn-primary btn-block"><i class="fa fa-print"></i> Cetak Laporan</button>
+					</div>
+				</div>
+			</div>
+			<?php
+			echo form_close() ?>
+		</div>
+		<!-- /.card-body -->
+	</div>
+	<!-- /.card -->
+</div>
+
+
+<div class="col-md-4">
+	<div class="card card-primary">
+		<div class="card-header">
+			<h3 class="card-title">Analisis Produk Bulanan</h3>
+		</div>
+		<div class="card-body">
+			<?php
+			echo form_open('analisis/produk_bulan') ?>
+			<div class="row">
+				<div class="col-sm-6">
+					<div class="form-group">
+						<label>Bulan</label>
+						<select name="bulan" class="form-control">
+							<?php
+							$mulai = 1;
+							for ($i = $mulai; $i < $mulai + 12; $i++) {
+								$sel = $i == date('Y') ? 'selected="selected"' : '';
+								echo '<option value="' . $i . '"' . $sel . '>' . $i . '</option>';
+							}
+							?>
+						</select>
+					</div>
+				</div>
+				<div class="col-sm-6">
+					<div class="form-group">
+						<label>Tahun</label>
+						<select name="tahun" class="form-control">
+							<?php
+							$mulai = date('Y') - 1;
+							for ($i = $mulai; $i < $mulai + 10; $i++) {
+								$sel = $i == date('Y') ? 'selected="selected"' : '';
+								echo '<option value="' . $i . '"' . $sel . '>' . $i . '</option>';
+							}
+							?>
+						</select>
+					</div>
+				</div>
+				<div class="col-sm-12">
+					<div class="form-group">
+						<button type="submit" class="btn btn-primary btn-block"><i class="fa fa-print"></i> Cetak Laporan</button>
+					</div>
+				</div>
+			</div>
+			<?php
+			echo form_close() ?>
+		</div>
+		<!-- /.card-body -->
+	</div>
+	<!-- /.card -->
+</div>
+
+
+<div class="col-md-4">
+	<div class="card card-primary">
+		<div class="card-header">
+			<h3 class="card-title">Analisis Produk Tahunan</h3>
+		</div>
+		<!-- /.card-header -->
+		<div class="card-body">
+			<?php
+			echo form_open('analisis/produk_tahun') ?>
+			<div class="row">
+				<div class="col-sm-12">
+					<div class="form-group">
+						<label>Tahun</label>
+						<select name="tahun" class="form-control">
+							<?php
+							$mulai = date('Y') - 1;
+							for ($i = $mulai; $i < $mulai + 10; $i++) {
+								$sel = $i == date('Y') ? 'selected="selected"' : '';
+								echo '<option value="' . $i . '"' . $sel . '>' . $i . '</option>';
+							}
+							?>
+						</select>
+					</div>
+				</div>
+				<div class="col-sm-12">
+					<div class="form-group">
+						<button type="submit" class="btn btn-primary btn-block"><i class="fa fa-print"></i> Cetak Laporan</button>
+					</div>
+				</div>
+			</div>
+			<?php
+			echo form_close() ?>
+		</div>
+		<!-- /.card-body -->
+	</div>
+	<!-- /.card -->
+</div>
